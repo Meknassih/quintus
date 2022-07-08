5ef40f66250b17f1d77c3f9f5b0f69340443e678
Hamza El Meknassi <elmeknassi.h@gmail.com>
2022-07-08 21:03:30 +0200

    Added selection logic

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -3,20 +3,25 @@ import { useState } from 'react';
 import WordleRow from './WordleRow';
 
 function Wordle() {
-  const [values, setValues] = useState([]);
-  function onClick() {
+  const [values, setValues] = useState(Array(6).fill("").map(() => Array(5).fill("")));
+  const [activeLetter, setActiveLetter] = useState([0, 0]);
 
+  function onClick(rowIndex, letterIndex) {
+    // console.log(`Clicked letter (${rowIndex},${letterIndex})`);
+    setActiveLetter([rowIndex, letterIndex])
   }
+
   function getSelectionForRow(rowIndex) {
-
+    return activeLetter[0] === rowIndex ? activeLetter[1] : null;
   }
+
   const rows = [];
   for (let i = 0; i < 6; i++) {
     rows.push(<WordleRow
       key={i}
       values={values}
-      selected={getSelectionForRow(0)}
-      onClick={onClick}
+      selected={getSelectionForRow(i)}
+      onClick={(letterIndex) => onClick(i, letterIndex)}
     />);
   }
 
